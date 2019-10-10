07cf8df125e2d0ce14fc71954b0d6a8931ae3702
QuocTuanJV <quoctuan.naf@gmail.com>
2019-10-10 21:20:39 +0700

    use Array Object to update and draw ball,paddle.

--- src/game.js
+++ src/game.js
@@ -7,14 +7,28 @@ function Game(gameWidth, gameHeight) {
         this.ball = new Ball(this);
         this.paddle = new Paddle(this);
 
+
+        this.gameObjects = [this.ball, this.paddle];
+
         new InputHandler(this.paddle);
     };
     this.updateGame = function (deltaTime) {
-        this.paddle.updatePositionPaddle(deltaTime);
-        this.ball.updatePositionBall(deltaTime);
+        // this.paddle.update(deltaTime);
+        // this.ball.update(deltaTime);
+
+        // this.gameObjects.forEach(object => object.update(deltaTime));
+        this.gameObjects.forEach(function (obj) {
+            obj.update(deltaTime);
+        });
+
     };
     this.drawGame = function (ctx) {
-        this.paddle.drawPaddle(ctx);
-        this.ball.drawBall(ctx);
+        // this.paddle.draw(ctx);
+        // this.ball.draw(ctx);
+        // this.gameObjects.forEach(object => object.draw(ctx));
+        this.gameObjects.forEach(function (obj) {
+            obj.draw(ctx);
+        });
+
     };
 }
